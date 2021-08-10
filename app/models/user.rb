cd16c5ea58668cb2af9cf351f618c55dce465614
Shakur <shakur.hassan@autolle.com>
2021-08-10 20:05:16 +0300

    installed active storate

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :lockable, :timeoutable, :trackable
+
+  has_many :posts
 end
