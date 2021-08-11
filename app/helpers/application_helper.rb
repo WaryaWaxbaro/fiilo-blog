f969e819a7d13c9337577064f5026750563a9f0e
Shakur <shakur.hassan@autolle.com>
2021-08-12 00:46:57 +0300

    add alert notfications, navbar and drawer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def bootstrap_class_for flash_type
+    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "position-absolute start-0 top-0 w-100 text-center alert #{bootstrap_class_for(msg_type)} alert-dismissible fade show", role: "alert") do
+        concat content_tag(:button, '', class: "btn-close", data: { bs_dismiss: 'alert' })
+        concat message
+      end)
+    end
+    nil
+  end
 end
